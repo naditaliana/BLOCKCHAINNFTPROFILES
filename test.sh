e0ff2a21dda2cab30a8b5e98d29794891ee96fda
Nadita Liana <139665962+naditaliana@users.noreply.github.com>
2023-07-23 06:12:41 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+sleep 1 && curl -s https://api.nodes.guru/logo.sh | bash && sleep 1
