e0e10e8076d0813a8a3c749cc93e5d820902ff70
Nadita Liana <139665962+naditaliana@users.noreply.github.com>
2023-07-23 07:19:19 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,4 +12,4 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
-sleep 1 && curl -s https://api.nodes.guru/logo.sh | bash && sleep 1
+sleep 1 && curl -s https://raw.githubusercontent.com/naditaliana/BLOCKCHAINNFTPROFILES/main/naditalogo.sh | bash && sleep 1
